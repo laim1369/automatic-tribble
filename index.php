d8bfbf7bcca289708de23b4449b94b072253c73c
laim1369 <eu.vlad@bk.ru>
2023-04-14 13:24:21 +0500

    insert student

--- index.php
+++ index.php
@@ -7,12 +7,22 @@
     <title>Клиент-серверное приложение</title>
 </head>
 <body>
-    <?php
 
-    define("HOST","localhost");
-    define("USER","root");
-    define("PASSWORD","root");
-    define("DB","school");
+    <form action="insertStudent.php" method="POST">
+        <input type="text" name="fname" id="fname" placeholder="введите имя" required><br>
+        <input type="text" name="lname" id="lname" placeholder="введите фамилию" required><br>
+        <input type="number" name="age" id="age" placeholder="введите возраст" required><br>
+        <input type="radio" name="gender" id="m" value="m" checked>
+        <label for="m">мужской</label>
+        <input type="radio" name="gender" id="f" value="f">
+        <label for="f">женский</label><br>
+        <input type="submit" value="добавить">
+    </form>
+
+
+
+    <?php
+    require_once ("config.php");
 
     // соединение с БД
 
